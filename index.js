7c541392ad09b5790632845e6dff00429cab05db
Nisanth Chunduru <chunisan@amazon.com>
2025-11-04 23:06:23 +0530

    Modify GET Health API endpoint to retrieve health from Dynamo

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const http = require("http");
-const PORT = process.env.PORT || 3000;
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
 
-const server = http.createServer((req, res) => {
+const PORT = process.env.PORT || 3000;
+const TABLE_NAME = "example-express-app-table";
+
+const client = new DynamoDBClient({});
+// const client = new DynamoDBClient({ region: "eu-north-1" });
+const docClient = DynamoDBDocumentClient.from(client);
+
+async function getDynamoItem(tableName, key) {
+  const command = new GetCommand({
+    TableName: tableName,
+    Key: key,
+  });
+  const response = await docClient.send(command);
+  if (!response.Item) {
+    throw new Error(`An item having key ${key} does not exist in ${tableName}`);
+  }
+  return response.Item;
+}
+
+async function getHealthDynamoItem() {
+  return getDynamoItem(TABLE_NAME, { pk: "health" });
+}
+
+async function getHealthStatus() {
+  const item = await getHealthDynamoItem();
+  return item.status;
+}
+
+const server = http.createServer(async (req, res) => {
   if (req.url === "/health" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ status: "ok" }));
+    try {
+      const status = await getHealthStatus();
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          status: status,
+        })
+      );
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          status: "error",
+          message: error.message,
+        })
+      );
+    }
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
